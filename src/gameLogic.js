305c14ebf787a1f0540d6546f3ff9e1a8736cc64
Pascal Thiel <pascalt0604@protonmail.com>
2023-08-22 13:51:10 +0200

    Implemented check for legal ship placement.

--- src/gameLogic.js
+++ src/gameLogic.js
@@ -10,7 +10,80 @@ const gameLogic = (() => {
     return char.toUpperCase().charCodeAt(0) - 'A'.charCodeAt(0);
   };
 
-  const isLegalShipPlacement = (x, y, ship, board) => {};
+  const isLegalShipPlacement = (x, y, ship, board) => {
+    const boardArr = board.getBoard();
+
+    if (!(x <= 9 && x >= 0)) {
+      return false;
+    }
+    if (!(y <= 9 && y >= 0)) {
+      return false;
+    }
+
+    if (ship.getDirection() === 'x') {
+      //Check own squares
+      if (x + ship.getLength() > 9) {
+        return false;
+      }
+      const startPos = x >= 1 ? x - 1 : x;
+      const endPos =
+        x + ship.getLength() <= 8
+          ? x + ship.getLength() + 1
+          : x + ship.getLength();
+      //Check own row
+      for (let i = startPos; i < endPos; i++) {
+        if (boardArr[i][y] != 'E') {
+          return false;
+        }
+      }
+      //Check row above
+      if (y > 0) {
+        for (let i = startPos; i < endPos; i++) {
+          if (boardArr[i][y - 1] != 'E') {
+            return false;
+          }
+        }
+      }
+      //Check row below
+      if (y < 9) {
+        for (let i = startPos; i < endPos; i++) {
+          if (boardArr[i][y + 1] != 'E') {
+            return false;
+          }
+        }
+      }
+    } else {
+      if (y + ship.getLength() > 9) {
+        return false;
+      }
+      const startPos = y >= 1 ? y - 1 : y;
+      const endPos =
+        y + ship.getLength() <= 8
+          ? y + ship.getLength() + 1
+          : y + ship.getLength();
+      for (let i = startPos; i < endPos; i++) {
+        if (boardArr[x][i] != 'E') {
+          return false;
+        }
+      }
+      if (x > 0) {
+        for (let i = startPos; i < endPos; i++) {
+          if (boardArr[x - 1][i] != 'E') {
+            return false;
+          }
+        }
+      }
+
+      if (x < 9) {
+        for (let i = startPos; i < endPos; i++) {
+          if (boardArr[x + 1][i] != 'E') {
+            return false;
+          }
+        }
+      }
+    }
+    return true;
+  };
   const isLegalAttack = (x, y, board) => {
     if (!(x <= 9 && x >= 0)) {
       return false;
--- src/gameLogic.test.js
+++ src/gameLogic.test.js
@@ -1,5 +1,7 @@
 import gameLogic from './gameLogic';
+import createGameboard from './gameboard';
 import createPlayer from './player';
+import createShip from './ship';
 
 describe('gameLogic', () => {
   it('Checks legal attacks accordingly', () => {
@@ -12,5 +14,22 @@ describe('gameLogic', () => {
 
     player2.takeTurn(player.getBoard(), 2, 7);
     expect(gameLogic.isLegalAttack(2, 7, player.getBoard())).toBe(false);
+  });
+
+  it('Checks legal ship placements accordingly', () => {
+    const board = createGameboard();
+    const ship1 = createShip(2);
+    const ship2 = createShip(3);
+    ship2.changeDirection();
+    expect(gameLogic.isLegalShipPlacement(9, 3, ship1, board)).toBe(false);
+    expect(gameLogic.isLegalShipPlacement(3, 9, ship2, board)).toBe(false);
+
+    expect(gameLogic.isLegalShipPlacement(1, 2, ship1, board)).toBe(true);
+    expect(gameLogic.isLegalShipPlacement(2, 5, ship2, board)).toBe(true);
+
+    board.placeShip(ship1, 1, 1);
+    expect(gameLogic.isLegalShipPlacement(2, 3, ship2, board)).toBe(false);
+    expect(gameLogic.isLegalShipPlacement(0, 0, ship2, board)).toBe(false);
+    expect(gameLogic.isLegalShipPlacement(2, 2, ship2, board)).toBe(false);
   });
 });
