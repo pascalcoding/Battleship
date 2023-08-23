24237d2a723bc72d3dd234be7188791d6c6b304e
Pascal Thiel <pascalt0604@protonmail.com>
2023-08-23 14:13:11 +0200

    Implemented random ship placement for computer.

--- src/computer.js
+++ src/computer.js
@@ -1,8 +1,20 @@
+import gameLogic from './gameLogic';
 import createGameboard from './gameboard';
+import createShip from './ship';
+
+const createShips = () => {
+  const ships = [];
+  for (let i = 2; i < 6; i++) {
+    ships.push(createShip(i));
+  }
+  ships.push(createShip(2));
+
+  return ships;
+};
 
 const createComputerPlayer = () => {
   const board = createGameboard();
-
+  const ships = createShips();
   const getBoard = () => board;
 
   const getRandomCoordinates = () => {
@@ -11,6 +23,14 @@ const createComputerPlayer = () => {
 
     return { x, y };
   };
+
+  ships.forEach((ship) => {
+    let { x, y } = getRandomCoordinates();
+    while (!gameLogic.isLegalShipPlacement(x, y, ship, board)) {
+      ({ x, y } = getRandomCoordinates());
+    }
+    board.placeShip(ship, x, y);
+  });
 
   const takeTurn = (enemyBoard) => {
     let legalMoveFound = false;
